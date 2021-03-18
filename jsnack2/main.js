aed57facb4aa11f23bad33be8144a34c2dd11d5c
wass01 <maatallawassim01@gmail.com>
2021-03-18 17:57:50 +0100

    snack2 terminato

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -4,6 +4,49 @@
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 $(document).ready(function() {
+  const squadre = [
+    {
+      nome: "Milan",
+      punti: 0,
+      falli: 0
+    },
+    {
+      nome: "Bayer Monaco",
+      punti: 0,
+      falli: 0
+    },
+    {
+      nome: "Chelsea",
+      punti: 0,
+      falli: 0
+    },
+    {
+      nome: "PSG",
+      punti: 0,
+      falli: 0
+    }
+  ];
 
+  let newDati = []
+
+  for (let i = 0; i < squadre.length; i++) {
+  squadre[i].punti = randomNumber(0, 100);
+  squadre[i].falli = randomNumber(0, 100);
+
+  const {nome, falli} = squadre[i];
+  newDati.push(nome, falli);
+}
+
+  console.log(newDati);
 
 });
+
+
+// functions
+function randomNumber(min, max) {
+  if (isNaN(min) || isNaN(max)) {
+    console.log("Non sono numeri");
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
